ad629ad3398e51fa2c8f1eaa33b1f649af5c0186
Ray Chung <raychungno1@gmail.com>
2021-11-06 10:35:17 -0400

    added initial cam files

--- productivity/routes.py
+++ productivity/routes.py
@@ -1,6 +1,12 @@
 from productivity import app
+from flask import Flask, render_template, Response
+from productivity.cam import gen_frames
 
 @app.route('/')
 @app.route('/home')
 def home_page():
-    return "hello world"
+    return render_template('camtest.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
