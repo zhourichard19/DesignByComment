99efde16e1238a877d00436c3fb713b4cfa8a1db
Ray Chung <raychungno1@gmail.com>
2021-11-06 12:22:17 -0400

    login functionality

--- productivity/__init__.py
+++ productivity/__init__.py
@@ -1,5 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = '2cfcaeb21b9cfdceda6fda8e'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app) # Hashes passwords
+login_manager = LoginManager(app)
+login_manager.login_view = 'login_page'
+login_manager.login_message_category = 'info'
 from productivity import routes
--- productivity/routes.py
+++ productivity/routes.py
@@ -1,6 +1,9 @@
-from productivity import app
-from flask import render_template, Response
+from productivity import app, db
+from flask import render_template, Response, redirect, url_for, flash
 from productivity.cam import camera, gen_frames
+from productivity.forms import RegisterForm, LoginForm
+from productivity.models import User
+from flask_login import login_user, logout_user, login_required, current_user
 
 @app.route('/')
 @app.route('/home')
@@ -10,3 +13,42 @@ def home_page():
 @app.route('/video_feed')
 def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register_page():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user_to_create = User(username=form.username.data,
+                              email_address=form.email_address.data,
+                              password=form.password1.data)
+        db.session.add(user_to_create)
+        db.session.commit()
+        login_user(user_to_create)
+        flash(f'Account created successfully! You are now logged in as {user_to_create.username}')
+        return redirect(url_for('home_page'))
+    if form.errors != {}: # If there are errors from the validations
+        for err_msg in form.errors.values():
+            flash(f'There was an error with creating a user: {err_msg}')
+
+    return render_template('register.html', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login_page():
+    form = LoginForm()
+    if form.validate_on_submit():
+        attempted_user = User.query.filter_by(username=form.username.data).first()
+        if attempted_user and attempted_user.check_password_correction(
+            attempted_password=form.password.data):
+            login_user(attempted_user)
+            flash(f'Success! You are logged in as: {attempted_user.username}')
+            return redirect(url_for('home_page'))
+        else:
+            flash('Username & password do not match! Please try again.')
+
+    return render_template('login.html', form=form)
+
+@app.route('/logout')
+def logout_page():
+    logout_user()
+    flash('You have been logged out!')
+    return redirect(url_for('home_page'))
