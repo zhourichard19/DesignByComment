79c43d5aed3b70850e8c55b97fbcf1b10ebda3c6
Ray Chung <raychungno1@gmail.com>
2021-11-07 05:34:01 -0500

    initial pi chart

--- productivity/static/predictionLoop.js
+++ productivity/static/predictionLoop.js
@@ -1,20 +1,42 @@
 let totalFocussed = 0;
 let totalDistractions = 0;
-let running = true;
-var intervalID = setInterval(function () {
-  $.get("/distractionCount", function (data) {
-    console.log(data);
-    totalFocussed += data.prediction;
-    totalDistractions += Math.abs(data.prediction - 1);
-    $(".prediction").html("Focussed: " + totalFocussed + ", Distractions: " + totalDistractions);
-  });
+let percentageDistraction=0;
+let percentageFocus=0
+var intervalID = setInterval(function(){
+    $.get( "/distractionCount", function( data ) {
+        console.log(data);
+        totalDistractions += data.prediction;
+        totalFocussed += Math.abs(data.prediction-1);
+        percentageDistraction=totalDistractions/(totalFocussed+totalDistractions);
+        percentageFocus=totalFocussed/(totalFocussed+totalDistractions);
+        var data = {
+          labels: [ 'Focused','Distracted'],
+          series: [totalFocussed,totalDistractions ]
+        };
+        var options = {
+          labelInterpolationFnc: function(value) {
+            return value[0]
+          }
+        };
+
+        var responsiveOptions = [
+          ['screen and (min-width: 640px)', {
+            chartPadding: 30,
+            labelOffset: 100,
+            labelDirection: 'explode',
+            labelInterpolationFnc: function(value) {
+              return value;
+            }
+          }],
+          ['screen and (min-width: 1024px)', {
+            labelOffset: 80,
+            chartPadding: 20
+          }]
+        ];
+
+        new Chartist.Pie('.ct-chart', data, options, responsiveOptions);
+
+
+        $( ".prediction" ).html("Focussed: "+totalFocussed+", Distractions: "+totalDistractions);
+      });
 }, 100);
-$(".buttonReset").bind("click", function () {
-  if (running) {
-    clearInterval(intervalID);
-    console.log("cleared");
-  } else {
-    console.log("TODO");
-  }
-  running = !running;
-});
