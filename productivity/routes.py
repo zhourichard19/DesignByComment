404a094abc33c4120c3bfb71d229f96e15f02a76
Ray Chung <raychungno1@gmail.com>
2021-11-06 23:04:04 -0400

    login page format

--- productivity/routes.py
+++ productivity/routes.py
@@ -12,6 +12,7 @@ import time
 def home_page():
     return render_template('index.html')
 
+
 @app.route('/video_feed')
 def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
